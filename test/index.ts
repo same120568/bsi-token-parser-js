b27de9e05d69509199f80514cfc0b9e8ffec7f81
Matt Gibbs <matt.gibbs@microfocus.com>
2017-11-02 12:18:22 -0400

    Flesh out unit tests

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { BsiTokenParser } from '../index';
+import { BsiTokenParser, IBsiToken } from '../index';
 
 const parser = new BsiTokenParser();
 {
@@ -26,7 +26,6 @@ const parser = new BsiTokenParser();
 
 {
     const bsiUrl2 = 'http://127.0.0.1:8888/bsi2.aspx?tid=2222&tc=SourceControl&pv=7812&payloadType=ANALYSIS_PAYLOAD&astid=7&ts=PHP';
-
     describe(`parsing "${bsiUrl2}"`, () => {
 
         const token = parser.parse(bsiUrl2);
@@ -50,12 +49,51 @@ const parser = new BsiTokenParser();
         it('should at least parse without crashing', () => {
             parser.parse(bsiToken1);
         });
+    });
+}
 
-        // it('should parse tokens and provide defaults where data is missing', () => {
-        //     const token = parser.parse(bsiToken1);
+{
+    const bsiToken2 = 'eyJ0ZW5hbnRJZCI6MSwidGVuYW50Q29kZSI6IlRlbmFudDEiLCJyZWxlYXNlSWQiOjMyMSwicGF5bG9hZFR5cGUiOiJBTkFMWVNJU19QQVlMT0FEIiwiYXNzZXNzbWVudFR5cGVJZCI6OCwidGVjaG5vbG9neVR5cGUiOiJKQVZBL0oyRUUiLCJ0ZWNobm9sb2d5VHlwZUlkIjo3LCJ0ZWNobm9sb2d5VmVyc2lvbiI6IjEuOSIsInRlY2hub2xvZ3lWZXJzaW9uSWQiOjE3LCJhdWRpdFByZWZlcmVuY2UiOiJBdXRvbWF0ZWQiLCJhdWRpdFByZWZlcmVuY2VJZCI6MiwiaW5jbHVkZVRoaXJkUGFydHkiOmZhbHNlLCJpbmNsdWRlT3BlblNvdXJjZUFuYWx5c2lzIjpmYWxzZSwic2NhblByZWZlcmVuY2UiOiIwIiwic2NhblByZWZlcmVuY2VJZCI6MCwicG9ydGFsVXJpIjoiaHR0cDovL2ZvZC5sb2NhbGhvc3QiLCJhcGlVcmkiOiJodHRwOi8vYXBpLmZvZC5sb2NhbGhvc3QifQ==';
+    describe(`parsing "${bsiToken2}"`, () => {
 
-        //     expect(token.apiUri).to.equal('https://api.ams.fortify.com');
-        // });
+        it('should at least parse without crashing', () => {
+            parser.parse(bsiToken2);
+        });
         
+        it('should have a Technology Type of "JAVA/J2EE"', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.technologyType).to.equal("JAVA/J2EE");
+        });
+
+        it('should have a Technology Version of "1.9"', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.technologyVersion).to.equal("1.9");
+        })
+
+        it('should have an Audit Preference of "Automated"', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.auditPreference).to.equal("Automated");
+            expect(token.auditPreferenceId).to.equal(2);
+        });
+
+        it('should not include third-party library scanning', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.includeThirdParty).to.equal(false);
+        });
+
+        it('should have an Assessment Type ID of "8"', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.assessmentTypeId).to.equal(8);
+        });
+        
+        it('should have an API URI of "http://api.fod.localhost"', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.apiUri).to.equal("http://api.fod.localhost");
+        });
+
+        it('should have a Portal URI of "http://fod.localhost"', () => {
+            const token = parser.parse(bsiToken2);
+            expect(token.portalUri).to.equal("http://fod.localhost");
+        });
     });
 }
